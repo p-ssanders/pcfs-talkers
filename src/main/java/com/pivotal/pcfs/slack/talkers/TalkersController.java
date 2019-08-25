a73482c8e95d790aa064b5ab7f136f9e691c4222
Sam Sanders <ssanders@pivotal.io>
2019-08-25 14:51:08 -0600

    refactor into named private methods that do one thing

--- src/main/java/com/pivotal/pcfs/slack/talkers/TalkersController.java
+++ src/main/java/com/pivotal/pcfs/slack/talkers/TalkersController.java
@@ -1,8 +1,13 @@
 package com.pivotal.pcfs.slack.talkers;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
+
 import java.util.Collection;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,32 +24,46 @@ public class TalkersController {
 
   @RequestMapping("/visualize")
   public String visualize(Model model) {
-    Collection<SlackMessage> slackChannelMessageHistory = slackService
-        .getChannelMessageHistory(PCFS_INTERNAL);
+    Collection<SlackMessage> slackChannelMessageHistory = slackService.getChannelMessageHistory(PCFS_INTERNAL);
 
-    Map<String, Integer> slackChannelParticipantCharacterCountByUserId =
-        slackChannelMessageHistory.stream()
-            .filter(slackMessage -> slackMessage != null && slackMessage.getUser() != null)
-            .collect(
-                Collectors.groupingBy(
-                    SlackMessage::getUser,
-                    Collectors.reducing(0, (user) -> user.getContent().length(), Integer::sum)
-                )
-            );
-
-    Map<String, Integer> slackChannelParticipantCharacterCountByUserName = 
-        slackChannelParticipantCharacterCountByUserId
-        .entrySet().stream()
-        .collect(Collectors.toMap(
-            e -> slackService.getUserRealName(e.getKey()),
-            e -> e.getValue()
-        ));
+    Map<String, Integer> realNameToTotalCharacterCount = mapRealNameToTotalCharacterCount(slackChannelMessageHistory);
 
     model.addAttribute("slackChannelName", "pcfs-internal");
-    model.addAttribute("slackChannelParticipantCharacterCount",
-        slackChannelParticipantCharacterCountByUserName);
+    model.addAttribute("slackChannelParticipantCharacterCount", realNameToTotalCharacterCount);
 
     return "visualize";
   }
 
+  private Map<String, Integer> mapRealNameToTotalCharacterCount(Collection<SlackMessage> slackChannelMessageHistory) {
+    Map<String, Integer> realNameToTotalCharacterCountMap =
+        slackChannelMessageHistory.stream()
+            .filter(notNull())
+            .filter(userNotNull())
+            .collect(groupByRealNameAndTotalCharacterCount());
+
+    return realNameToTotalCharacterCountMap;
+  }
+
+  private Predicate<SlackMessage> notNull() {
+    return slackMessage -> slackMessage != null;
+  }
+
+  private Predicate<SlackMessage> userNotNull() {
+    return slackMessage -> slackMessage.getUser() != null;
+  }
+
+  private Collector<SlackMessage, ?, Map<String, Integer>> groupByRealNameAndTotalCharacterCount() {
+    return groupingBy(
+        this::getSlackUserRealName,
+        reducing(0, getSlackMessageLength(), Integer::sum)
+    );
+  }
+
+  private String getSlackUserRealName(SlackMessage slackMessage) {
+    return slackService.getUserRealName(slackMessage.getUser());
+  }
+
+  private Function<SlackMessage, Integer> getSlackMessageLength() {
+    return (slackMessage) -> slackMessage.getContent().length();
+  }
 }
